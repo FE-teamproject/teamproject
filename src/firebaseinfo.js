a4c35a5e77b01d549b56fcb199d2f3c02126db50
SEOYUN KIM <nado0903@gmail.com>
2021-08-23 02:22:42 +0900

    chore: Add firebase

---
+++ src/firebaseinfo.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+// Add the Firebase products that you want to use
+import 'firebase/analytics';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA77lYjYSAzU1Mr6_d-gaCyKKdBLBc2VvY',
+  authDomain: 'dailyauction-544ae.firebaseapp.com',
+  projectId: 'dailyauction-544ae',
+  storageBucket: 'dailyauction-544ae.appspot.com',
+  messagingSenderId: '471359378705',
+  appId: '1:471359378705:web:23bc1f7466ce8663dd68b0',
+  measurementId: 'G-03PPZSNMZK',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const authService = firebase.auth();
