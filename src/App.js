7aadeffabe2509abf887da02d64fd1d389d4f065
yhana972 <yhana972@gmail.com>
2021-08-31 12:33:14 +0900

    dundu : bootstrap 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 import { fire } from 'firebase';
 
 function App() {
-  return <div>Daily Auction</div>;
+  return (
+    <div className="container-sm">
+      <h2>Daily Auction</h2>
+      <Button>Boot strap</Button>
+    </div>
+  );
 }
 
 export default App;
